abb1dd36281f8b019abe64c3a23681f99d3dc173
hoeck <dfw407so@dd.bib.de>
2010-02-07 10:38:34 +0100

    first cut at 1.4 compatible table-view editors and renderers

--- src/hoeck/pivot/content.clj
+++ src/hoeck/pivot/content.clj
@@ -1,276 +1,55 @@
 
 (ns hoeck.pivot.content
-  (:use clojure.contrib.except)
+  (:use clojure.contrib.except
+        hoeck.pivot
+        hoeck.pivot.components
+        hoeck.pivot.listeners)
   (:import (org.apache.pivot.wtk Keyboard Keyboard$KeyCode
                                  TableView$CellRenderer TableView$RowEditor)
            (org.apache.pivot.util Vote)
            (java.text DateFormat DecimalFormat)))
 
-;; impose a loading order because there is a circular reference between content and components
-(declare label-cell-renderer)
-(declare table-view-editor)
-(use 'hoeck.pivot.components)
-(use 'hoeck.pivot.listeners)
-(use 'hoeck.pivot.datastructures)
-(require '[hoeck.pivot.relations :as pivot-rel])
+;; protocols to define editors and renderers
+(defprotocol HasComponent
+  (component [c] "returns the pivot component"))
 
-;; table-view cell renderers and editors
-;; require tableData to be a List of tuples (clojure hashmaps)
-;; see hoeck.pivot.relations
+(defprotocol Editor
+  (on-open [e] "Called after opening the popup window")
+  (value [e] "value of the edit component"))
 
-(defn set-cell-renderer-styles
-  "given the argument map of table-view-cell-renderer, set the columns
-  font and color styles according to the styles defined in the table-view."
-  [args]
-  (let [tv (or (:table-view args) (throwf ":table-view missing in cell-renderer"))
-        cmp (or (:component args) (throwf ":component missing in cell-renderer"))
-        tv-styles (.getStyles tv)
-        color (.get tv-styles
-                    (if (and (.isEnabled tv) (not (:row-disabled? args)))
-                      (if (:row-selected? args)
-                        (if (.isFocused tv)
-                          "selectionColor"
-                          "inactiveSelectionColor")
-                        "color")
-                      "disabledColor"))]
-    (doto (.getStyles cmp)
-      (.put "color" color)
-      (.put "font" (.get tv-styles "font")))))
+(defprotocol Renderer
+  (render [r argm] "renders the renderer"))
 
-(defn cell-value [col tuple default]
-  (get tuple (keyword (get-property col :name)) default))
+(defn split-key-pairs [s]
+  (loop [s s
+         opts {}]
+    (let [[k v & t] s]
+      (cond (keyword? k) (recur t (assoc opts k v))
+            :else [opts s]))))
 
-(defn fixed-label-cell-renderer [text]
-  (fn ([] (label :styles {:horizontal-alignment :left
-                          :vertical-alignment :center
-                          :color [0 0 255]
-                          :padding [2 2 2 2]}))
-    ([{:keys [component, value, column] :as args}]
-       (set-cell-renderer-styles args)
-       (set-property component :text text)
-       (set-property component :styles {:color [0 0 255]
-                                        :text-decoration :underline}))))
+(defn- find-ctor-def
+  "Search a deftype body for a ctor definition and return it."
+  [macro-body]
+  (loop [b macro-body
+         body nil]
+    (let [[h & t] b]
+      (cond (nil? b) [nil body]
+            (keyword? h) (recur (next t) (concat body (list h) (first t)))
+            (symbol? h) (recur t b)
+            (seq? h) (if (or (vector? (first h)) (seq? (first h)))
+                       [h (concat body t)]
+                       (recur t (concat body (list h))))
+            :else (throwf "no matching clause in find-ctor-code")))))
 
-(defn label-cell-renderer
-  "like a plain cell renderer but renders data from a clojure hashmap instead
-  of a pivot dictionary."
-  []
-  (fn ([] (label :styles {:horizontal-alignment :left
-                          :vertical-alignment :center
-                          :padding [2 2 2 2]}))
-    ([{:keys [component, value, column] :as args}]
-       (set-cell-renderer-styles args)
-       (set-property component :text (str (cell-value column value ""))))))
-
-(defn label-map-cell-renderer
-  "like a label-cell-renderer, but renders values found in display-fn after
-  calling (display-fn the-cell-value)."
-  [display-fn]
-  (fn ([] (label :styles {:horizontal-alignment :left
-                          :vertical-alignment :center
-                          :padding [2 2 2 2]}))
-    ([{:keys [component, value, column] :as args}]
-       (set-cell-renderer-styles args)
-       (set-property component :text (str (display-fn (cell-value column value nil)))))))
-
-(defn label-timestamp-cell-renderer
-  "Render a java.sql.timestamp"
-  []
-  (fn ([] (label :styles {:horizontal-alignment :left
-                          :vertical-alignment :center
-                          :padding [2 2 2 2]}))
-    ([{:keys [component, value, column] :as args}]
-       (set-cell-renderer-styles args)
-       (let [cv (cell-value column value nil)]
-         (when cv (set-property 
-                   component :text 
-                   (str (-> (DateFormat/getDateTimeInstance DateFormat/MEDIUM DateFormat/SHORT)
-                            (.format cv)))))))))
-
-(defn list-button-renderer
-  []
-  (fn ([] (list-button))
-    ([{:keys [component, value, column] :as args}]
-       (set-property component :data (str (cell-value column value ""))))))
-
-;; table-view-editors
-
-;; basically:
-;;  create a component
-;;  open a popup window with the component as the only content
-;;  register listeners:
-;;    to close the popup on any non-popup-click or ESC
-;;    to update the row on enter (or doubleclick)
-;;    etc. pp.
-
-(defn scroll-to-cell
-  "Scroll the tableview so that the cell [row, col]-idx is visible.
-  Return the bounds of this cell as [x y w h]."
-  [tv row-idx col-idx]
-  (let [cb (.getCellBounds tv row-idx col-idx)]
-    (.scrollAreaToVisible tv cb)
-    (get-bounds (.getVisibleArea tv cb))))
-
-(defn table-view-editor-popup
-  "Prepare everything to host the edit-component in a popup above the given
-  table-cell ad row-idx and col-idx.
-  On user cancel commands (ESC, clicked outside) and table-data-changes, 
-  close the popup and cancel the editor.
-  Returns the popup window."
-  [edit-component #^::TableViewEditor editor tv cell-bounds]
-  (let [popup (window :auxilliary true
-                      :location (let [[x y w h] cell-bounds] 
-                                  [x (+ y (/ (- h (.getPreferredHeight edit-component -1))
-                                             2))])
-                      edit-component)
-        ;; listeners (require references to the other listeners)
-        cml (container-mouse-listener {cont :container [x y] :int}
-              :mouse-down (let [w (.getComponentAt cont x y)]
-                            (when-not (identical? popup w)
-                              ;; user clicked out of the bounds of this popup
-                              (.cancel editor))
-                            false)
-              :mouse-wheel true
-              false)
-        ;; cancel editing if any rows are modified while editing
-        tvrl (table-view-row-listener _ (.cancel editor))
-        ;;  cancel editing on any table-data change
-        tvl (table-view-listener {m :method}
-              (when (#{:table-data-changed
-                       :table-view-row-editor-changed}
-                     m)
-                (.cancel editor)))
-        ;; cancel on ESCAPE, save on ENTER
-        ckl (component-key-listener {keycode :int :as args}
-              :key-pressed (do (condp = keycode
-                                 Keyboard$KeyCode/ESCAPE (.cancel editor)
-                                 Keyboard$KeyCode/ENTER (.save editor)
-                                 :ignore)
-                               false)
-              false)
-        ;; popup window-listener: edit initialization & finalization
-        wsl (window-state-listener {w :window :as args}
-              :window-opened (do (add-listener (.getDisplay w) cml)
-                                 (add-listener tv tvl tvrl))
-              :window-closed (do (remove-listener (:display args) cml)
-                                 (remove-listener tv tvl tvrl)
-                                 (.requestFocus tv))
-              :preview-window-open Vote/APPROVE
-              :preview-window-close Vote/APPROVE)]
-    (add-listener edit-component ckl)
-    (add-listener popup wsl)
-    (.open popup (.getWindow tv))
-    (.requestFocus edit-component)
-    popup))
-
-(defn find-table-view-cell
-  "return [name value] of a given table-view-cell at [row,column]"
-  [tv row-idx col-idx]
-  (let [colname (-> tv .getColumns (.get col-idx) (get-property :name) keyword)]
-    [colname (get (.get (.getTableData tv) row-idx) colname)]))
-  
-(deftype TableViewEditor [state editor] :as this
-  TableView$RowEditor
-  (edit [tv row-idx col-idx]
-    (let [[colname colvalue] (find-table-view-cell tv row-idx col-idx)
-          b (scroll-to-cell tv row-idx col-idx)
-          _ (reset! state {:tv tv :row-idx row-idx :col-idx col-idx :editor this
-                           :colname colname :colvalue colvalue :bounds b})
-          e (editor :edit @state) ;; nil means do-not-edit
-          p (when e (table-view-editor-popup e this tv b))]
-      (if e 
-        (swap! state assoc :popup p :component e)
-        (.cancel this))))
-  (cancel []
-    (if-let [p (:popup @state)] (.close p))
-    (reset! state nil))
-  (save []
-    (let [{:keys [colname row-idx tv] :as s} @state]
-      (pivot-rel/update (.getTableData tv)
-                        row-idx
-                        {colname (editor :value s)})
-      (.cancel this)))
-  (isEditing []
-             (not (empty? @state))))
-
-(defn table-view-editor
-  "ctor for a table-view editor"
-  [f]
-  (TableViewEditor (atom {}) f))
-
-(defmacro defeditor
-  "Define a function that returns a TableView$RowEditor.
-  Docstring is mandatory."
-  [editor-name docstring argvec & body]
-  (let [st (gensym 'state)]
-    `(defn ~editor-name
-       ~docstring
-       ~argvec
-       (fn table-view-editor-fn [method# ~st]
-         (condp = method#
-           ~@(mapcat (fn [[method-symbol bind-expr & method-body]]
-                       `(~(-> method-symbol name keyword)
-                         (let ~(conj bind-expr st)
-                           ~@method-body)))
-                     body))))))
-
-(defeditor exception-editor
-  "editor that throws an exception when its invoked."
-  [msg]
-  (edit [_] (throwf msg))
-  (value [_] nil))
-
-(defeditor no-editor
-  "editor to not edit a column"
-  []
-  (edit [_] nil)
-  (value [_] nil))
-
-(defeditor text-input-editor
-  "Uses a text-input to edit the cell. Stores values as strings."
-  []
-  (edit [{n :colname v :colvalue [x y w h] :bounds}]
-        (doto (text-input :text (str v) :preferred-width w)
-          (.selectAll)))
-  (value [{ti :component}] (get-property ti :text)))
-
-(defeditor list-button-editor
-  "Uses a list-button to choose one from a map of choices. A map value is
-  displayed if its key is the current value of the cell.
-  If choice-map a function of one argument, the current tuple and it
-   should return a map to lookup the displayed-value."
-  [choice-map]
-  (edit [{n :colname v :colvalue [x y w h] :bounds tv :tv row-idx :row-idx}]
-        (let [choice-map (if (fn? choice-map)
-                           (choice-map (.get (.getTableData tv) row-idx))
-                           choice-map)
-              lb (list-button :data (get choice-map v)
-                              :preferred-width w
-                              :list-data (make-list (vals choice-map)))]
-          (add-listener lb (list-button-selection-listener _
-                             (pivot-rel/update (.getTableData tv)
-                                               row-idx
-                                               {n (let [s (.getSelectedItem lb)
-                                                           i (first (filter #(-> % val (= s)) choice-map))]
-                                                       (key i))})))
-          lb))
-  (value [_] 'never-called))
-
-(defn editors
-  "Editor that invokes a different editor for each column.
-  Use 'default to supply a default editor."
-  ([columnkey editor & more]
-     (editors (apply hash-map columnkey editor more)))
-  ([column-editor-map]
-     (let [default (get column-editor-map 'default (no-editor))]
-       (fn table-view-editor-fn [m {n :colname :as state}]
-         ((get column-editor-map n default) m state)))))
-
-
-;; button renderers
-
-
-
-
+(defmacro deftypec
+  "Creates a type with a custom constructor function. The constructor is
+  a method form without a name in the regular deftype body."
+  [name fields & body] ;; nonstandard ctor
+  (let [[ctor-def deftype-body] (find-ctor-def body)]
+    `(do (deftype ~name ~fields ~@deftype-body)
+         (let [original-ctor# ~name
+               nonstandard-ctor# (fn ~@ctor-def)]
+           (defn ~name [& args#]
+             (apply original-ctor# (apply nonstandard-ctor# args#)))))))
 
 
---
+++ src/hoeck/pivot/content/table_view.clj
@@ -0,0 +1,276 @@
+
+(ns hoeck.pivot.content.table-view
+  (:use clojure.contrib.except
+        clojure.contrib.pprint
+        hoeck.pivot
+        hoeck.pivot.content ;; editor
+        hoeck.pivot.components
+        hoeck.pivot.listeners
+        hoeck.pivot.datastructures)
+  (:import (org.apache.pivot.wtk Keyboard Keyboard$KeyCode
+                                 TableView, TableView$Column
+                                 TableView$CellRenderer TableView$RowEditor
+                                 TableView$RowEditor$RowEditorListenerList)
+           (org.apache.pivot.util Vote)
+           (java.text DateFormat DecimalFormat)))
+
+;;; table-view-editor
+
+(defn reposition [tv-editor]
+  (let [{:keys [tv row-index col-index edit-component popup]} @(.state tv-editor)
+        ;; Get the cell bounds
+        cb (let [cb (.getCellBounds tv row-index col-index)]
+             (.scrollAreaToVisible tv cb)
+             (.getVisibleArea cb))
+        coords (.mapPointToAncestor tv (.getDisplay tv)
+                                   (.x cb)
+                                   (.y cb))]
+    ;; Position the popup/editor to fit over the cell bounds
+    (set-property edit-component :preferred-width (.width cb))
+    (set-property popup :location
+                  [(.x coords)
+                   (+ (.y coords)
+                      (/ (- (.height cb)
+                            (get-property edit-component :preferred-height))
+                         2))])))
+
+(defn basic-editor-setup
+  "installs listeners in the popup-window, the popups parent window
+  and in the table-view."
+  [e edit-component]
+  (let [tv (:table-view @(:state e))
+        edit-component (-> e :state deref :editor component)
+        popup (window edit-component)
+        mouse-l (container-mouse-listener
+                 {c :container [x y] :int}
+                 :mouse-down (do (when-not (= (.getComponentAt x y)
+                                              popup)
+                                   (.saveChanges e))
+                                 false)
+                 :mouse-wheel true
+                 false)
+        comp-l (component-listener
+                _
+                :size-changed (reposition e)
+                :location-changed (reposition e))
+        tv-l (table-view-listener
+              _
+              :table-data-changed (.cancelEdit e)
+              :row-editor-changed (.cancelEdit e))
+        tv-row-l (table-view-row-listener _ (.cancelEdit e))
+        popup-l (window-state-listener
+                 {w :window, d :display, o :owner}
+                 :preview-window-open Vote/APPROVE
+                 :preview-window-close Vote/APPROVE
+                 :window-opened
+                 (do (add-listener (get-property w :display) mouse-l)
+                     (add-listener tv comp-l tv-l tv-row-l))
+                 :window-closed
+                 (do (remove-listener d mouse-l)
+                     (remove-listener tv comp-l tv-l tv-row-l)
+                     (.moveToFront o)
+                     (reset! (:state e) nil)))]
+    (add-listener popup popup-l)
+    (add-listener edit-component
+                  (component-key-listener
+                   {keycode :int}
+                   :key-pressed (if (= keycode Keyboard$KeyCode/ENTER)
+                                  (.saveChanges e)
+                                  (.cancelEdit e))))
+    popup-l))
+
+(defn get-tuple [tv row-idx]
+  (.get (get-property tv :data) row-idx))
+
+(defn get-column-key [tv col-idx]
+  (let [c (.get (get-property tv :cols) col-idx)]
+    (keyword (get-property c :name))))
+
+(deftype TableViewEditor [editor-ctor state] :as this
+  TableView$RowEditor
+  (editRow [tv, row-index, col-index]
+    (let [ll (TableView$RowEditor$RowEditorListenerList.)
+          vote (.previewEditRow ll row-index col-index)]
+      (if (= vote Vote/APPROVE)
+        (let [editor (editor-ctor
+                      (let [k (get-column-key tv col-index)
+                            tuple ])
+                      {:table-view tv
+                       :tuple (get-tuple tv row-index)
+                       :value (get-tuple tv row-index)
+                       :key 
+                       :row-idx row-index
+                       :col-idx col-index})]
+          (reset! state (atom {:tv tv
+                               :row-idx row-index
+                               :col-idx col-index
+                               :editor editor}))
+          (let [popup (basic-editor-setup this)]
+            (swap! state assoc :popup popup)
+            (.open popup (get-property tv :window))
+            (on-open editor)
+            (.rowEditing ll this tv row-index col-index)))
+        (.editRowVetoed ll this vote))))
+  (getRowEditorListeners [] (:row-editor-listeners @state))
+  (cancelEdit
+   []
+   (-> @state :popup .close)
+   (-> @state
+       :row-editor-listeners
+       (.editCancelled this (:tv @state) (:row-idx @state) (:col-idx @state)))
+   (reset! state {}))
+  (isEditing [] (boolean (:editing @state)))
+  (saveChanges
+   []
+   (let [{:keys [tv row-idx col-idx
+                 row-editor-listeners
+                 editor]} @state
+         changes (make-dictionary
+                  {(get-column-key tv col-idx)
+                   (value editor)})
+         vote (.previewSaveChanges this tv row-idx col-idx changes)]
+     (if (= vote Vote/APPROVE)
+       (let [tup (merge (get-tuple tv row-idx) changes)
+             data (get-property tv :data)]
+         ;; Notifying the parent will close the popup
+         (if-not (.getComparator data)
+           (.put data row-idx tup)
+           (do (.remove data row-idx 1)
+               (.add data tup)
+               (let [i (.indexOf data tup)]
+                 (set-property tv :selected-index i)
+                 (.scrollAreaToVisible tv (.getRowBounds tv i)))))
+         (.changesSaved row-editor-listeners this tv row-idx col-idx))
+       (.saveChangesVetoed row-editor-listeners this vote)))))
+
+(defn table-view-editor [editor-ctor]
+  (TableViewEditor editor-ctor (atom {})))
+
+(defproperties TableView [tv]
+  :editor (.setRowEditor tv it) (.getRowEditor tv)
+  "The row editor, see hoeck.pivot.content for implementations.")
+
+
+;; renderer
+
+  ;; "Returns a TableView$CellRenderer implemented through the render-f function.
+  ;; Calling render-f without an argument should return a component (i.e. a Label)
+  ;; which is used to render data cells.
+  ;; Upon rendering, render-f is called with a map of arguments:
+  ;;   :component :value :table-view :column 
+  ;;   :row-selected? :row-highlighted? :row-disabled?
+  ;; and should set the given :component according to its needs, eg:
+  ;;   (when row-highlighted? (label :self (:component args) :font [\"Arial\" :bold 12]))."
+(deftype TableViewCellRenderer [r]
+  TableView$CellRenderer
+  ;; TableView$CellRenderer
+  ;;void render(Object value, TableView tableView, TableView.Column column, boolean rowSelected, boolean rowHighlighted, boolean rowDisabled)
+  ;;void render(Object value, int rowIndex, int columnIndex, TableView tableView, String columnName, boolean rowSelected, boolean rowHighlighted, boolean rowDisabled) 
+  (render [value,
+           row-idx,
+           col-idx,
+           table-view,
+           column-name,
+           row-selected?,
+           row-highlighted?,
+           row-disabled?]
+          (render r {:value value
+                     :row-idx row-idx
+                     :col-idx col-idx
+                     :table-view table-view
+                     :column-name column-name
+                     :row-selected? row-selected?
+                     :row-highlighted? row-highlighted?
+                     :row-disabled? row-disabled?})
+          nil)
+  ;; Renderer   
+  ;;Dictionary<String,Object> getStyles() Returns the renderer's style dictionary.
+  (getStyles [] (.getStyles (component r)))
+  ;; ConstrainedVisual    
+  ;;int getPreferredHeight(int width) Returns the visual's preferred height given the provided width constraint.
+  (getPreferredHeight [width] (.getPreferredHeight (component r) width))
+  ;;Dimensions getPreferredSize() Returns the visual's unconstrained preferred size.
+  (getPreferredSize [] (.getPreferredSize (component r)))
+  ;;int getPreferredWidth(int height) Returns the visual's preferred width given the provided height constraint.
+  (getPreferredWidth [height] (.getPreferredWidth (component r) height))
+  ;;void setSize(int width, int height) Sets the visual's render size.
+  (setSize [width height] (.setSize (component r) width height))
+  (getBaseline [width height] (.getBaseLine (component r) width height))
+  ;; Visual
+  ;;int getHeight() Returns the visual's height.
+  (getHeight [] (.getHeight (component r)))
+  ;;int getWidth() Returns the visual's width.
+  (getWidth [] (.getWidth (component r)))
+  ;;void paint(Graphics2D graphics) Paints the visual.
+  (paint [graphics] (.paint (component r) graphics))
+  (getBaseline [] (.getBaseLine (component r))))
+
+(defn set-cell-renderer-styles
+  "given the argument map of table-view-cell-renderer, set the columns
+   font and color styles according to the styles defined in the table-view."
+  [args]
+  (let [tv (or (:table-view args) (throwf ":table-view missing in cell-renderer"))
+        cmp (or (:component args) (throwf ":component missing in cell-renderer"))
+        tv-styles (.getStyles tv)
+         color (.get tv-styles
+                     (if (and (.isEnabled tv) (not (:row-disabled? args)))
+                       (if (:row-selected? args)
+                         (if (.isFocused tv)
+                           "selectionColor"
+                           "inactiveSelectionColor")
+                         "color")
+                       "disabledColor"))]
+    (doto (.getStyles cmp)
+      (.put "color" color)
+      (.put "font" (.get tv-styles "font")))))
+
+(defn table-view-cell-renderer [renderer]
+  (Table-View-Cell-Renderer (renderer)))
+
+
+;; editor-dispatch, each col his own editor
+
+(defn column-dispatch-editor
+  "an editor which looks in the current columns userdata for a suitable
+  ::editor."
+  []
+  (fn [{n :column-name
+        tv :table-view
+        :as args}]
+    (when-let [ed (-> tv
+                      (get-property :columns)
+                      (get n)
+                      (get-property :editor))]
+      (ed args))))
+
+(defproperties TableView$Column [tc]
+  :editor
+  (.setUserdata tc (assoc (or (.getUserData tc) {})
+                     ::editor it))
+  (get (.getUserData tc) ::editor)
+  "Sets the editor to user for this column, requires a proper table-view :editor to be set.")
+
+
+;;; editor & renderer implementations
+
+(deftypec text-input-editor [ti]
+  ([{v :value}] [(text-input :text v)]) ;; ctor
+  HasComponent
+  (component [] ti)
+  Editor
+  (value [] (get-property ti :text))
+  (on-open [] (doto ti
+                  (.selectAll)
+                  (.requestFocus))))
+
+(deftypec text-renderer [ti]
+  ([] [(text-input)])
+  HasComponent
+  (component [] ti)
+  Renderer
+  (render [argm]
+          (set-cell-renderer-styles ti argm)))
+
+
+
+
