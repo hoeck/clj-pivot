b1016bc83f9b7b32c1121e4ef2c12237ffc89c57
hoeck <dfw407so@dd.bib.de>
2010-10-14 07:37:25 +0200

    Synced project.clj and build.xml, changed pivot dep to look at maven central instead of clojars.
    Use Lein for dependency resolution and basic clj-pivot
    compilation. Use ant to compile the examples and generate the jars.

--- project.clj
+++ project.clj
@@ -1,19 +1,18 @@
-
-(defproject clj-pivot "0.2"
-    :description "A Clojure library for Apache Pivot"
-    :url "http://github.com/hoeck/clj-pivot"
-    :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojars.hoeck/apache-pivot-wtk-terra "1.5.1"]]
-    ;; compile everything except tools
-    :namespaces (hoeck.pivot
-                 hoeck.pivot.components.TreeBranch
-                 hoeck.pivot.components
-                 hoeck.pivot.listeners
-                 hoeck.pivot.forms
-                 hoeck.pivot.icons
-                 hoeck.pivot.datastructures
-                 hoeck.pivot.content
-                 hoeck.pivot.content.table-view
-                 hoeck.pivot.Application))
+(defproject clj-pivot "0.2.0"
+  :description "A Clojure library for Apache Pivot"
+  :url "http://github.com/hoeck/clj-pivot"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.apache.pivot/pivot-wtk "1.5.1"]
+                 [org.apache.pivot/pivot-wtk-terra "1.5.1"]]
+  :namespaces [hoeck.pivot
+               hoeck.pivot.components.TreeBranch
+               hoeck.pivot.components
+               hoeck.pivot.listeners
+               hoeck.pivot.forms
+               hoeck.pivot.icons
+               hoeck.pivot.datastructures
+               hoeck.pivot.content
+               hoeck.pivot.content.table-view
+               hoeck.pivot.Application])
 
 
